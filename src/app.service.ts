477d01825f22939fe4426c387ed11e45717b235b
littlehorseboy <littlehorseboy@gmail.com>
2020-06-19 22:25:34 +0800

    feat(): cors 全予以通過 date-fns，subDays 讓 api 回傳的日期資料都是昨天

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { format, subDays } from 'date-fns';
+
 import { PunchIn } from './punchIn.model';
 
 @Injectable()
 export class AppService {
   getHello(): PunchIn[] {
+    const date = new Date();
+
+    const subOneDayDate = subDays(date, 1);
+
     return [
       {
-        date: '1090618',
+        date: `${Number(format(subOneDayDate, 'yyyy')) - 1911}${format(subOneDayDate, 'MMdd')}`,
         time: '0750',
         periodsOfTheDay: 'morning',
       },
       {
-        date: '1090618',
+        date: `${Number(format(subOneDayDate, 'yyyy')) - 1911}${format(subOneDayDate, 'MMdd')}`,
         time: '1850',
         periodsOfTheDay: 'afternoon',
       },
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin(origin, callback) {
+      callback(null, true);
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
